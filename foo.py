625061be87459594ab47cb1326452fd983600eaa
wlnirvana <weilin1990@gmail.com>
2016-02-10 22:53:49 +0000

    added multiples.py

--- foo.py
+++
@@ -1,9 +0,0 @@
-# blah blah
-def foobar():
-    """docstring"""
-    bar = 5
-    for i in range(bar):
-        print i
-
-def foo():
-    bar()
---
+++ multiples.py
@@ -0,0 +1,4 @@
+number = 5
+for multiple in range(1,13):
+    answer = number * multiple
+    print("{} x {} = {}".format(number,multiple,answer))
