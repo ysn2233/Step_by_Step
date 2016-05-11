0292260a1e53a2dca58f8f8ba351972191e027d9
Lulai Zhu <lz3510@new-temp>
2016-05-11 18:55:08 +0100

    Only include instructor.py for coverage

--- unittest_coverage.sh
+++ unittest_coverage.sh
@@ -7,5 +7,5 @@ coverage erase --rcfile=$CONFIG
 rm -rf unittest_htmlcov
 
 coverage run --rcfile=$CONFIG --branch -a test_instructor.py
-coverage report --rcfile=$CONFIG -m
+coverage report --rcfile=$CONFIG
 coverage html --rcfile=$CONFIG
