f66f30bc80237f5e83cf04833a300759513aaccb
Lulai Zhu <lz3510@new-temp>
2016-05-15 14:58:49 +0100

    Modified draw_house.py

--- modules/turtle.mod
+++ modules/turtle.mod
@@ -7,7 +7,7 @@ backward                Move turtle [obj] backwards by [arg1] steps
 left                    Turn turtle [obj] left by [arg1] degrees
 right                   Turn turtle [obj] right by [arg1] degrees
 goto                    Move turtle [obj] to position ([arg1], [arg2])
-speed					Set moving speed of turtle [obj] to [arg1]
-position				position of turtle [obj]
-shape					Set the shape of [obj]
-circle					Move turtle [obj] around a circle
+speed                   Set moving speed of turtle [obj] to [arg1]
+position                position of turtle [obj]
+shape                   Change turtle [obj] shape to a [arg1]
+circle                  Move turtle [obj] around a circle
--- sample_inputs/draw_house.py
+++ sample_inputs/draw_house.py
@@ -2,28 +2,47 @@ import math
 import turtle
 
 t = turtle.Turtle()
+t.shape('triangle')
+t.speed(0)
 
-def drawHouse():
+def draw_window(length):
+    "Draw a window of given width and height with a cross in the center"
+
+    start_x, start_y = t.position()
+
+    tiles_count = 2
+    tile_length = length // tiles_count
+
+    for x_step in range(tiles_count):
+        for y_step in range(tiles_count):
+            x = start_x + (x_step * tile_length)
+            y = start_y + (y_step * tile_length)
+            jump_turtle(x,y)
+            draw_square(tile_length)
+
+def jump_turtle(x,y):
+    t.penup()
+    t.goto(x,y)
+    t.pendown()
+
+def draw_house():
     "Draw a house consisting of a square base roof and a number of windows"
-    drawSquare(100)
-    drawRoof(100,50)
 
-    x,y = 25,-10
-    for i in range(2):
-        for j in range(2):
-            t.penup()
-            t.goto(x+i*60,y-j*60)
-            t.pendown()
-            drawWindow(12,16)
+    draw_square(100)
+    draw_roof(100,50)
 
-def drawSquare(length):
-    "Draw a square with given side length"
-    for i in range(4):
-        t.forward(length)
-        t.right(90)
+    start_x = 20
+    start_y = -25
+    for x_step in range(2):
+        for y_step in range(2):
+            x = start_x + x_step * 60
+            y = start_y - y_step * 60
+            jump_turtle(x,y)
+            draw_window(16)
 
-def drawRoof(width,height):
+def draw_roof(width,height):
     "Draw a triangular roof with given width and height"
+
     t.forward(width)
     radians = math.atan(height/(width/2))
     degrees = math.degrees(radians)
@@ -34,27 +53,11 @@ def drawRoof(width,height):
     t.forward(diagonal)
     t.right(degrees)
 
-def drawWindow(width,height):
-    "Draw a window of given width and height with a cross in the center"
-    t.forward(width/2)
-    t.left(90)
-    t.forward(height)
-    t.backward(height)
-    t.right(90)
-    t.forward(width/2)
-    t.left(90)
-    t.forward(height/2)
-    t.left(90)
-    t.forward(width)
-    t.backward(width)
-    t.right(90)
-    t.forward(height/2)
-    t.left(90)
-    t.forward(width)
-    t.left(90)
-    t.forward(height)
-    t.left(90)
+def draw_square(length):
+    "Draw a square with given side length"
 
-if (__name__ == "__main__"):
-    drawHouse()
-    input()
+    for i in range(4):
+        t.forward(length)
+        t.right(90)
+
+draw_house()
